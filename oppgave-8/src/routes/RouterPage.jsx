76d760f9dde1a2026594257aa2faa6d4a7a1ce1b
hardstein <89636138+hardstein@users.noreply.github.com>
2022-03-11 12:00:23 +0100

    Update RouterPage.jsx

--- oppgave-8/src/routes/RouterPage.jsx
+++ oppgave-8/src/routes/RouterPage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import App from "../App";
 import Home from "../pages/Home";
 
 function RouterPage() {
